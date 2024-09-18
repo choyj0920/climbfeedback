a5b9f1d6858912a2609a254a6b1966e296ca745a
Ji Hun Lee <75845270+easy-hun@users.noreply.github.com>
2024-09-18 21:33:10 +0900

    Update VideoFragment.kt

--- app/src/main/java/com/hunsu/climbfeedback/mainfrag/VideoFragment.kt
+++ app/src/main/java/com/hunsu/climbfeedback/mainfrag/VideoFragment.kt
@@ -1,27 +1,35 @@
 package com.hunsu.climbfeedback.mainfrag
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hunsu.climbfeedback.VideoActivity
 import com.hunsu.climbfeedback.databinding.FragmentVideoBinding
-
 
 class VideoFragment : Fragment() {
 
-    private var binding: FragmentVideoBinding? =null
+    private var binding: FragmentVideoBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentVideoBinding.inflate(inflater, container, false)
 
-        binding=FragmentVideoBinding.inflate(inflater,container,false)
+        // 버튼 클릭 시 VideoActivity로 이동
+        binding?.selectButton?.setOnClickListener {
+            val intent = Intent(activity, VideoActivity::class.java)
+            startActivity(intent)
+        }
 
         return binding!!.root
-
     }
 
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
